1f1d14d543b2461f3bef23453af6cf6bf220ef53
Tony Skidmore <16082810+tonyskidmore@users.noreply.github.com>
2021-02-01 18:15:19 +0000

    Update solr_startup.sh

--- solr_startup.sh
+++ solr_startup.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+set -e
+echo "This runs at container start"
 
-# export SOLR_PORT=${PORT}
-export SOLR_PORT=8983
+# export SOLR_PORT=8983
 
 # starting sshd process
 # sed -i "s/SSH_PORT/$SSH_PORT/g" /etc/ssh/sshd_config
 sudo -n /usr/sbin/sshd
+
+export SOLR_PORT="$PORT"
+echo "SOLR_PORT=$PORT"
+touch /tmp/solr_startup.test
