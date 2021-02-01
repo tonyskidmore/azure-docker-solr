007ce8a04a59e1189f44d6141fa8ffeb0b9e8343
Tony Skidmore <16082810+tonyskidmore@users.noreply.github.com>
2021-02-01 17:32:53 +0000

    Update solr_startup.sh

--- solr_startup.sh
+++ solr_startup.sh
@@ -4,5 +4,5 @@
 export SOLR_PORT=8983
 
 # starting sshd process
-sed -i "s/SSH_PORT/$SSH_PORT/g" /etc/ssh/sshd_config
-/usr/sbin/sshd
+# sed -i "s/SSH_PORT/$SSH_PORT/g" /etc/ssh/sshd_config
+sudo -n /usr/sbin/sshd
